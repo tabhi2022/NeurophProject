29c976c1b273aca98f86e1e016c71c5c8dddd9af
HP <HP@DESKTOP-TBTT9L7>
2022-10-08 19:42:34 +0530

    Commit-01

---
+++ NeurophProject/src/myNeurophDLPackage/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package myNeurophDLPackage;
+
+/**
+ * @author HP
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Welcome to neuroph project");
+
+	}
+
+}
